4d2fb115568e64e8509184caaedab98e2fee447c
Karl Charles <karl.a.charles1996@gmail.com>
2019-07-09 00:46:08 +0100

    Deployer setup | Test Deploy 1

--- deploy.php
+++ deploy.php
@@ -4,7 +4,7 @@ namespace Deployer;
 require 'recipe/common.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'library');
 
 // Project repository
 set('repository', 'https://github.com/kcharlesbusiness/library.git');
@@ -21,28 +21,46 @@ set('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
 // Hosts
+host('library')
+  ->hostname('46.101.52.142')
+  ->user('root')
+  ->set('branch', 'master')
+  ->identityFile('~/.ssh/id_rsa')
+  ->set('deploy_path', '/var/www/html/library');
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
+task('build', function () {
+  run('cd ' . __DIR__ . '&& npm install && npm run build');
+})->local();
+
+task('upload', function () {
+  upload(__DIR__ . "/node_modules", '{{release_path}}/');
+});
+
+task('deploy:owner', function () {
+  run('chown -R www-data:www-data /var/www');
+});
     
 
 // Tasks
 
 desc('Deploy your project');
 task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
+  'deploy:info',
+  'deploy:prepare',
+  'deploy:lock',
+  'deploy:release',
+  'deploy:update_code',
+  'deploy:shared',
+  'deploy:writable',
+  'build',
+  'upload',
+  'deploy:vendors',
+  'deploy:clear_paths',
+  'deploy:symlink',
+  'deploy:owner',
+  'deploy:unlock',
+  'cleanup',
+  'success'
 ]);
 
 // [Optional] If deploy fails automatically unlock.
